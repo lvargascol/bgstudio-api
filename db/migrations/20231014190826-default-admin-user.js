ca0e13109cf1292044c38e7c7a1738b39b663c92
Leonardo Vargas <lvargascol>
2023-10-14 16:27:45 -0300

    [ADD] Migration with user by default

---
+++ db/migrations/20231014190826-default-admin-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const {
+  USER_TABLE,
+} = require('./../models/userModel');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    const hash = await bcrypt.hash('clave1234', 10);
+
+    await queryInterface.bulkInsert(USER_TABLE, [
+      {
+        email: 'lvargascol@gmail.com',
+        password: hash,
+        role: 'admin',
+        created_at: new Date()
+      }
+    ]);   
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete(USER_TABLE, 
+      {
+        id: 1,
+      })
+
+  }
+};
